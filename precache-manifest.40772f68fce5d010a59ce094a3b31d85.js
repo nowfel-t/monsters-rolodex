b0777860c80098159fae8f00b9ea9129f0b92d38
nowfel terki <nowfel.terki@tutanota.com>
2020-06-13 20:47:37 +0200

    Updates

---
+++ precache-manifest.40772f68fce5d010a59ce094a3b31d85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9225f1824a3f837e6c286fd203d08d7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2036599e275737c0b231",
+    "url": "/monsters-rolodex/static/css/main.77165e02.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2036599e275737c0b231",
+    "url": "/monsters-rolodex/static/js/main.4fe7f7d3.chunk.js"
+  },
+  {
+    "revision": "96e4dc8f64b3356b837b",
+    "url": "/monsters-rolodex/static/js/runtime-main.6b89a6eb.js"
+  }
+]);
